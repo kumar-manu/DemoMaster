23be1d192a07f12d24d494d58eab2e467c5b3894
Manu <manustar47@gmail.com>
2016-07-07 08:44:58 +0530

    Added comment manu

--- SeleniumProj/Tests/SampleTest.cs
+++ SeleniumProj/Tests/SampleTest.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
-
+//Manu
 namespace SeleniumProj.Tests
 {
     [TestClass]
