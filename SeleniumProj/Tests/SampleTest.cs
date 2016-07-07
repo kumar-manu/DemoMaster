81a4149fc0cf0915e70cf425640f7ec0e9562edb
Manu <manustar47@gmail.com>
2016-07-07 13:34:16 +0530

    removed comment

--- SeleniumProj/Tests/SampleTest.cs
+++ SeleniumProj/Tests/SampleTest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-//MJ
+//
 namespace SeleniumProj.Tests
 {
     [TestClass]
